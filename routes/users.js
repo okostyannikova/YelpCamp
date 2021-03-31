d2a3ac3a33c5874592621a7fb1e6470a62db4e7f
o.kostyannikova <o.kostyannikova@gmail.com>
2021-03-31 19:01:39 +0300

    Refactoring: split routers and controllers

--- routes/users.js
+++ routes/users.js
@@ -1,56 +1,25 @@
 const express = require("express");
-const passport = require("passport");
-const User = require("../models/user");
-const catchAsync = require("../utils/catchAsync");
 const router = express.Router();
+const passport = require("passport");
+const catchAsync = require("../utils/catchAsync");
+const users = require("../controllers/users");
 
-router.get("/register", (req, res) => {
-  res.render("users/register");
-});
+router
+  .route("/register")
+  .get(users.renderRegister)
+  .post(catchAsync(users.register));
 
-router.post(
-  "/register",
-  catchAsync(async (req, res) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    users.login
+  );
 
-      req.login(registeredUser, (err) => {
-        if (err) return next(err);
-      });
-
-      req.flash("Success", "Welcome to Yelp Camp!");
-      res.redirect("/campgrounds");
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/register");
-    }
-  })
-);
-
-router.get("/login", (req, res) => {
-  res.render("users/login");
-});
-
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/login",
-  }),
-  (req, res) => {
-    req.flash("success", "welcome back!");
-    const redirectUrl = req.session.returnTo || "/campgrounds";
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-  }
-);
-
-router.get("/logout", (req, res) => {
-  req.logout();
-  req.flash("success", "Goodbye!");
-  res.redirect("/campgrounds");
-});
+router.get("/logout", users.logout);
 
 module.exports = router;
