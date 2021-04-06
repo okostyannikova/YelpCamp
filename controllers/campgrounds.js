9e7a577459e56f9917f9919132ec8a239b85d3f7
o.kostyannikova <o.kostyannikova@gmail.com>
2021-04-06 15:45:32 +0300

    Add photo upload

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require("../models/campground");
+const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -10,10 +11,11 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res, next) => {
-  // if (!req.body.campground) {
-  //   throw new ExpressError("Invalid Campground Data", 400);
-  // }
   const campground = new Campground(req.body.campground);
+  campground.images = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
   campground.author = req.user._id;
   await campground.save();
 
@@ -37,7 +39,7 @@ module.exports.showCampground = async (req, res) => {
   res.render("campgrounds/show", { campground });
 };
 
-module.exports.renderEditFrom = async (req, res) => {
+module.exports.renderEditForm = async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   if (!campground) {
     req.flash("error", "Cannot find that campground!");
@@ -48,9 +50,27 @@ module.exports.renderEditFrom = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
   const { id } = req.params;
-  await Campground.findByIdAndUpdate(id, {
+  const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
+
+  const images = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
+
+  campground.images.push(...images);
+  await campground.save();
+
+  if (req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
+    }
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+  }
+
   req.flash("success", "Successfully updated campground!");
   res.redirect(`/campgrounds/${id}`);
 };
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,14 +2,18 @@ const express = require("express");
 const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
-
+const multer = require("multer");
+const { storage } = require("../cloudinary");
 const { isLoggedIn, validateCampground, isAuthor } = require("../middleware");
+
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
   .post(
     isLoggedIn,
+    upload.array("image"),
     validateCampground,
     catchAsync(campgrounds.createCampground)
   );
@@ -22,6 +26,7 @@ router
   .put(
     isLoggedIn,
     isAuthor,
+    upload.array("image"),
     validateCampground,
     catchAsync(campgrounds.updateCampground)
   )
@@ -31,7 +36,7 @@ router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
-  catchAsync(campgrounds.renderEditForm)
+  catchAsync(campgrounds.renderEditForm),
 );
 
 module.exports = router;
